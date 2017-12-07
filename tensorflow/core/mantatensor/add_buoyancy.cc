2d742c0f4c15a4eef44a9fda633b4e6c584139e4
Markus Ansorge <Markus.Ansorge@gmx.de>
2017-12-07 16:36:13 +0100

    Added dimension asserts for AddBuoyancyOP

--- tensorflow/core/mantatensor/add_buoyancy.cc
+++ tensorflow/core/mantatensor/add_buoyancy.cc
@@ -14,9 +14,9 @@ using namespace tensorflow;
 
 
 REGISTER_OP("AddBuoyancy")
+    .Input("in_vel_grid: float")
     .Input("flags_grid: int32")
     .Input("density_grid: float")
-    .Input("in_vel_grid: float")
     .Input("force: float")
     .Output("out_vel_grid: float")
     .SetShapeFn([](::tensorflow::shape_inference::InferenceContext* c) {
@@ -162,7 +162,18 @@ struct AddBuoyancy<CPUDevice> {
   }
 };
 
+#include <iostream>
+#include <initializer_list>
 
+
+bool isSameValue(std::initializer_list<long> list)
+{
+
+    for (int i = 1; i < list.size(); i++)
+        if(list.begin()[0] != list.begin()[i])
+            return false;
+    return true;
+}
 
 template <typename Device>
 class AddBuoyancyOp : public OpKernel {
@@ -171,22 +182,43 @@ class AddBuoyancyOp : public OpKernel {
 
   void Compute(OpKernelContext* context) override {
     // Grab the input tensor
-    const Tensor& flag_grid_tensor = context->input(0);
-    auto flag_grid_flat = flag_grid_tensor.flat<int>();
 
-    const Tensor& density_tensor = context->input(1);
-    auto density_flat = density_tensor.flat<float>();
 
-    const Tensor& input_vel_tensor = context->input(2);
+    const Tensor& input_vel_tensor = context->input(0);
     TensorShape vel_shape = input_vel_tensor.shape();
     auto input_vel_flat = input_vel_tensor.flat<float>();
+
+    const Tensor& flag_grid_tensor = context->input(1);
+    TensorShape flag_grid_shape = flag_grid_tensor.shape();
+    auto flag_grid_flat = flag_grid_tensor.flat<int>();
+
+    const Tensor& density_tensor = context->input(2);
+    TensorShape density_shape = density_tensor.shape();
+    auto density_flat = density_tensor.flat<float>();
 
     const Tensor& force_tensor = context->input(3);
     TensorShape force_shape = force_tensor.shape();
 
-    // Create an output tensor
-    Tensor* output_vel_tensor = NULL;
-    OP_REQUIRES_OK(context, context->allocate_output(0, vel_shape, &output_vel_tensor));
+    OP_REQUIRES(context, vel_shape.dims() == 5,
+                 errors::InvalidArgument("AddBuoyancy expects as first parameter a 5-D float velocity array: batches, width, height, depth, dimension"));
+    OP_REQUIRES(context, flag_grid_shape.dims() == 4,
+                 errors::InvalidArgument("AddBuoyancy expects as second parameter a 4-D int flags array: batches, width, height, depth"));
+    OP_REQUIRES(context, density_shape.dims() == 4,
+                 errors::InvalidArgument("AddBuoyancy expects as third parameter a 4-D float density array: batches, width, height, depth"));
+    OP_REQUIRES(context, force_shape.dims() == 1,
+                 errors::InvalidArgument("AddBuoyancy expects as fourth parameter a 1-D float force array: dimension"));
+
+
+    OP_REQUIRES(context, isSameValue({vel_shape.dim_size(0), flag_grid_shape.dim_size(0), density_shape.dim_size(0)}),
+                 errors::InvalidArgument("AddBuoyancy expects that the batch size is equal for all inputs"));
+    OP_REQUIRES(context, isSameValue({vel_shape.dim_size(1), flag_grid_shape.dim_size(1), density_shape.dim_size(1)}),
+                 errors::InvalidArgument("AddBuoyancy expects that the width size is equal for all inputs"));
+    OP_REQUIRES(context, isSameValue({vel_shape.dim_size(2), flag_grid_shape.dim_size(2), density_shape.dim_size(2)}),
+                 errors::InvalidArgument("AddBuoyancy expects that the height size is equal for all inputs"));
+    OP_REQUIRES(context, isSameValue({vel_shape.dim_size(3), flag_grid_shape.dim_size(3), density_shape.dim_size(3)}),
+                 errors::InvalidArgument("AddBuoyancy expects that the depth size is equal for all inputs"));
+    OP_REQUIRES(context, isSameValue({vel_shape.dim_size(4), force_shape.dim_size(0)}),
+                 errors::InvalidArgument("AddBuoyancy expects that the dimension size is equal for all inputs"));
 
     FluidGrid fluidGrid;
     fluidGrid.vel = input_vel_flat.data();
@@ -198,6 +230,12 @@ class AddBuoyancyOp : public OpKernel {
     fluidGrid.height    = vel_shape.dim_size(2);
     fluidGrid.depth     = vel_shape.dim_size(3);
     fluidGrid.dim       = force_shape.dim_size(0);
+
+
+    // Create an output tensor
+    Tensor* output_vel_tensor = NULL;
+    OP_REQUIRES_OK(context, context->allocate_output(0, vel_shape, &output_vel_tensor));
+
 
     AddBuoyancy<Device>()(
         context->eigen_device<Device>(),
